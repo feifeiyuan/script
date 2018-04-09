9728069837b386df507fb48998b93475d1962cc1
feifeiyuan <33796779+feifeiyuan@users.noreply.github.com>
2018-04-09 15:22:55 +0800

    judge_cpu_core_num

---
+++ judge_diff_cpu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cpu_path="/sys/devices/system/cpu/"
+cpu_core_cnt=0
+
+function cpu_core {
+	cpu_core_cnt=0
+	# 这个用来判断cpu的core num应该是更加合理的
+	while [ -d ${cpu_path}cpu${cpu_core_cnt} ]
+	do
+		let "cpu_core_cnt++"
+	done
+}
+
+
